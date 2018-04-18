efd3a6a9778fc4b66a25f0d624e8196686a1eada
Ivan Echevarria <iechevarria@email.wm.edu>
2018-04-17 22:30:54 -0400

    add histogram array counts

--- index.js
+++ index.js
@@ -7,57 +7,58 @@ let es = require('event-stream');
 let Stream = require('stream').Stream;
 
 module.exports = (opts) => {
-    if (!opts) opts = {};
-    if (!opts.cols) opts.cols = 80;
-    
-    let c = charm();
-    let bufs = buffers();
-    
-    let ws = es.writeArray(function (err, bufs) {
-        let data = buffers(bufs).slice();
-        let png = new PNG(data);
+  if (!opts) opts = {};
+  if (!opts.cols) opts.cols = 80;
+  if (!opts.rows) opts.rows = 20;
+  
+  let c = charm();
+  let bufs = buffers();
+  
+  let ws = es.writeArray(function (err, bufs) {
+    let data = buffers(bufs).slice();
+    let png = new PNG(data);
+
+    png.decode(function (pixels) {
+      let r = new Array(256).fill(0);
+      let g = new Array(256).fill(0);
+      let b = new Array(256).fill(0);
+
+      for (let y = 0; y < png.height; y++) {
+        for (let x = 0; x < png.width; x++) {
+          let i = (y * png.width + x) * 4;
+          if (pixels[i + 3] > 0) {
+            r[pixels[i]]++;
+            g[pixels[i + 1]]++;
+            b[pixels[i + 2]]++; 
+          }
+        }
+      }
+
+      console.log(Math.max(Math.max.apply(null, r), Math.max.apply(null, g), Math.max.apply(null, b)));
+
+      let rc = new Array(opts.cols).fill(0);
+      let rg = new Array(opts.cols).fill(0);
+      let rb = new Array(opts.cols).fill(0);
+      let cts = new Array(opts.cols).fill(0);
+      // if bucket has three, multiply by 4/3 to preserve continuity
+      for (let k = 0; k < 256; k++) {
         
-        // png.decode(function (pixels) {
-        //     let dx = png.width / opts.cols;
-        //     let dy = 2 * dx;
-            
-        //     for (let y = 0; y < png.height; y += dy) {
-        //         for (let x = 0; x < png.width; x += dx) {
-        //             let i = (Math.floor(y) * png.width + Math.floor(x)) * 4;
-                    
-        //             let ix = x256([ pixels[i], pixels[i+1], pixels[i+2] ]);
-        //             if (pixels[i+3] > 0) {
-        //                 c.background(ix).write(' ');
-        //             }
-        //             else {
-        //                 c.display('reset').write(' ');
-        //             }
-        //         }
-        //         c.display('reset').write('\r\n');
-        //     }
-            
-        //     c.display('reset').end();
-        // });
-        png.decode(function (pixels) {
-            let r = new Array(256).fill(0);
-            let g = new Array(256).fill(0);
-            let b = new Array(256).fill(0);
+      }
 
-            for (let y = 0; y < png.height; y++) {
-                for (let x = 0; x < png.width; x++) {
-                    let i = (y * png.width + x) * 4;
-                    r[pixels[i]] += 1;
-                    g[pixels[i + 1]] += 1;
-                    b[pixels[i + 2]] += 1; 
-                }
-            }
+      for (let i = 0; i < opts.rows; i++) {
+        for (let j = 0; j < opts.cols; j++) {
+          // console.log("r[" + i + "]: " +  "-".repeat(80 * r[i] / Math.max.apply(null, r)));
+          c.background(x256([255, 0, 0])).write(' ');
+        }
+        c.display('reset').write('\r\n');
+      }
 
-            let m = Math.max(Math.max.apply(null, r), Math.max.apply(null, g), Math.max.apply(null, b));
-            console.log(m);
-        });
+      // let dy = Math.max(Math.max.apply(null, r), Math.max.apply(null, g), Math.max.apply(null, b)) / opts.rows;
+      // let dx = png.width / opts.cols;
 
-        
-    });
-    
-    return es.duplex(ws, c);
+      c.display('reset').end();
+    });  
+  });
+  
+  return es.duplex(ws, c);
 };
